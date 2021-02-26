28043cff0e5e75de1b879b78b43c2ef1e25d1759
Jose Ugia <ugia@google.com>
2021-02-26 13:56:21 +0100

    Use commas to control indent styles

--- pay/example/lib/main.dart
+++ pay/example/lib/main.dart
@@ -57,41 +57,53 @@ class _PaySampleAppState extends State<PaySampleApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('T-shirt Shop'),
-        ),
-        backgroundColor: Colors.white,
-        body:
-            ListView(padding: EdgeInsets.symmetric(horizontal: 20), children: [
+      appBar: AppBar(
+        title: const Text('T-shirt Shop'),
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        children: [
           Container(
-              margin: const EdgeInsets.symmetric(vertical: 20),
-              child: Image(
-                  image: AssetImage('assets/images/ts_10_11019a.jpg'),
-                  height: 350)),
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            child: Image(
+              image: AssetImage('assets/images/ts_10_11019a.jpg'),
+              height: 350,
+            ),
+          ),
           Text(
             'Amanda\'s Polo Shirt',
             style: TextStyle(
-                fontSize: 20,
-                color: Color(0xff333333),
-                fontWeight: FontWeight.bold),
+              fontSize: 20,
+              color: Color(0xff333333),
+              fontWeight: FontWeight.bold,
+            ),
           ),
           SizedBox(height: 5),
           Text(
             '\$50.20',
-            style: TextStyle(color: Color(0xff777777), fontSize: 15),
+            style: TextStyle(
+              color: Color(0xff777777),
+              fontSize: 15,
+            ),
           ),
           SizedBox(height: 15),
           Text(
             'Description',
             style: TextStyle(
-                fontSize: 15,
-                color: Color(0xff333333),
-                fontWeight: FontWeight.bold),
+              fontSize: 15,
+              color: Color(0xff333333),
+              fontWeight: FontWeight.bold,
+            ),
           ),
           SizedBox(height: 5),
           Text(
-              'A versatile full-zip that you can wear all day long and even...',
-              style: TextStyle(color: Color(0xff777777), fontSize: 15)),
+            'A versatile full-zip that you can wear all day long and even...',
+            style: TextStyle(
+              color: Color(0xff777777),
+              fontSize: 15,
+            ),
+          ),
           SizedBox(height: 15),
           Center(child: _getPaymentButtons())
         ]));
