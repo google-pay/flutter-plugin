50b5e375c039a04f8e83551c044450eb1a56666c
Jose Ugia <ugia@google.com>
2021-02-26 13:59:46 +0100

    Create a widget that wraps the management of the client and payment configuration inside of the button

--- pay/lib/pay.dart
+++ pay/lib/pay.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:pay_mobile/pay_mobile.dart';
 import 'package:pay_platform_interface/pay_platform_interface.dart';
@@ -12,3 +13,4 @@ export 'package:pay_mobile/pay_mobile.dart'
     show GooglePayButton, GooglePayButtonColor, GooglePayButtonType;
 
 part 'src/pay.dart';
+part 'src/widgets/google_pay_button_widget.dart';
---
+++ pay/lib/src/widgets/google_pay_button_widget.dart
@@ -0,0 +1,61 @@
+part of '../../pay.dart';
+
+typedef PayGestureTapCallback = void Function(Pay client);
+
+class GooglePayButtonWidget extends StatelessWidget {
+  final Pay _googlePayClient;
+  final GooglePayButton _googlePayButton;
+
+  final Widget _childOnError;
+  final Widget _loadingIndicator;
+
+  GooglePayButtonWidget._(Key key, this._googlePayClient, this._googlePayButton,
+      this._childOnError, this._loadingIndicator)
+      : super(key: key);
+
+  factory GooglePayButtonWidget({
+    Key key,
+    @required paymentConfigurationAsset,
+    @required PayGestureTapCallback onPressed,
+    type,
+    color,
+    childOnError,
+    loadingIndicator,
+  }) {
+    Pay googlePayClient = Pay.fromAsset(paymentConfigurationAsset);
+    GooglePayButton googlePayButton = GooglePayButton(
+      onPressed: () => onPressed(googlePayClient),
+      type: type,
+      color: color,
+    );
+
+    return GooglePayButtonWidget._(
+      key,
+      googlePayClient,
+      googlePayButton,
+      childOnError,
+      loadingIndicator,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _googlePayClient.userCanPay(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.data == true) {
+            return SizedBox(
+              width: double.infinity,
+              child: this._googlePayButton,
+            );
+          } else if (snapshot.hasError) {
+            return this._childOnError;
+          }
+        }
+
+        return this._loadingIndicator;
+      },
+    );
+  }
+}
