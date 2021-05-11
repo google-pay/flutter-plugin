c67f11d96e15601f91301f7c10bd225dcea6aff2
Jose Ugia <ugia@google.com>
2021-05-11 23:08:01 +0200

    Support multiple payment providers under the same client

--- pay/lib/src/pay.dart
+++ pay/lib/src/pay.dart
@@ -10,35 +10,42 @@ const supportedProviders = {
 
 class Pay {
   final PayPlatform _payPlatform;
-  Future? _initializationFuture;
-  late final PaymentConfiguration _configuration;
+  late final List<PaymentConfiguration> _configurations;
+  late final _assetInitializationFuture;
 
-  Pay._(Map<String, dynamic> paymentConfiguration)
+  Pay(this._configurations) : _payPlatform = PayMethodChannel();
+
+  Pay.fromAssets(List<String> configAssets)
       : _payPlatform = PayMethodChannel() {
-    _configuration = PaymentConfiguration.fromMap(paymentConfiguration);
+    _assetInitializationFuture = _loadConfigAssets(configAssets);
   }
 
-  }
+  Future _loadConfigAssets(List<String> configurationAssets) async =>
+      _configurations = await Future.wait(
+          configurationAssets.map((ca) => PaymentConfiguration.fromAsset(ca)));
 
+  PaymentConfiguration _findConfig([PayProvider? provider]) => provider == null
+      ? _configurations.first
+      : _configurations.firstWhere((c) => c.provider == provider);
 
-
-  Future<bool> userCanPay() async {
-    // Wait for the client to finish instantiation before issuing calls
-    await _initializationFuture;
+  Future<bool> userCanPay([PayProvider? provider]) async {
+    await _assetInitializationFuture;
+    final configuration = _findConfig(provider);
 
     if (supportedProviders[defaultTargetPlatform]!
-        .contains(_configuration.provider.toSimpleString())) {
-      return _payPlatform.userCanPay(await _getPaymentData());
+        .contains(configuration.provider.toSimpleString())) {
+      return _payPlatform.userCanPay(configuration);
     }
 
     return Future.value(false);
   }
 
   Future<Map<String, dynamic>> showPaymentSelector({
+    PayProvider? provider,
     required List<PaymentItem> paymentItems,
   }) async {
-    await _initializationFuture;
-    final paymentData = await _getPaymentData();
-    return _payPlatform.showPaymentSelector(paymentData, paymentItems);
+    await _assetInitializationFuture;
+    final configuration = _findConfig(provider);
+    return _payPlatform.showPaymentSelector(configuration, paymentItems);
   }
 }
