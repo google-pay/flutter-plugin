621b183ae307c7d9a5ba35e48ae445039989c683
Jose Ugia <ugia@google.com>
2022-01-04 15:00:59 +0100

    Remove unnecessary test_driver

--- pay/example/test_driver/integration_test.dart
+++
@@ -1,6 +0,0 @@
-/// Copyright 2021 Google LLC.
-/// SPDX-License-Identifier: Apache-2.0
-
-import 'package:integration_test/integration_test_driver.dart';
-
-Future<void> main() => integrationDriver();
