1323120c5b00806b1b3707583ead2c6d9c60d1e5
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-12-20 19:40:04 +0300

    remove dark theme from demo app

--- pay/example/lib/main.dart
+++ pay/example/lib/main.dart
@@ -46,7 +46,6 @@ class PayMaterialApp extends StatelessWidget {
         const Locale('de', ''),
       ],
       theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
       home: PaySampleApp(),
     );
   }
